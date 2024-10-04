b76d552157fac1560e252cb033517a17c6ca62e6
dejvidEm <bithug10@gmail.com>
2024-10-04 19:16:59 +0200

    test

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = 443; // HTTPS port, o SSL sa stará proxy
+const PORT = 4433; // HTTPS port, o SSL sa stará proxy
 
 // Testovacia trasa pre overenie
 app.get('/test', (req, res) => {
-  res.json({ message: 'Backend is working on port 443!' });
+  res.json({ message: 'Backend is working on port 4433!' });
 });
 
 // Spustenie servera na porte 443
