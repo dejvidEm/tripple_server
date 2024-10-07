11a73bead2c64c06d3bedfc8a5d8f27d192f848b
dejvidEm <bithug10@gmail.com>
2024-10-08 01:52:25 +0200

    update https

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
 // Middleware pre parsing JSON
 app.use(express.json());
+
+// Nahrávanie SSL certifikátu
+const options = {
+  key: fs.readFileSync('~/ssl/localhost.key'),
+  cert: fs.readFileSync('~/ssl/localhost.crt'),
+};
 
 // Definovanie základnej cesty
 app.get('/', (req, res) => {
@@ -13,14 +21,12 @@ app.get('/', (req, res) => {
 
 // Pridanie ďalšej cesty pre testovanie
 app.post('/auth/register', (req, res) => {
-
   const data = req.body;
-
   console.log('Dostal som tieto dáta: ', data);
   res.send('Data received - dostal som všetky dáta potrebné na auth!');
 });
 
-// Spustenie servera
-app.listen(PORT, () => {
-  console.log(`Server beží na http://localhost:${PORT}`);
+// Spustenie HTTPS servera
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server beží na https://localhost:${PORT}`);
 });
