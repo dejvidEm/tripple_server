0d853203267d137de7200f54ad03772d76b0340e
dejvidEm <bithug10@gmail.com>
2024-10-08 03:33:48 +0200

    update cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
@@ -13,17 +14,32 @@ const options = {
   cert: fs.readFileSync('/home/david/ssl/localhost.crt'),
 };
 
+// Dovolené domény na requesty
+const allowedOrigins = ['https://tripple-beryl.vercel.app'];
+
+// Nastavenie CORS middleware pre všetky cesty
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Pridaj OPTIONS metódu
+  credentials: true, // Ak chceš povoliť cookies a autentifikačné hlavičky
+}));
+
 // Definovanie základnej cesty
 app.get('/', (req, res) => {
   console.log('Prijatá požiadavka na základnú cestu');
   res.send('Backend server fungujeeee!');
 });
 
-// Pridanie ďalšej cesty pre testovanie
+// Pridanie ďalších ciest pre testovanie
 app.post('/auth/register', (req, res) => {
   const data = req.body;
   console.log('Dostal som tieto dáta: ', data);
   res.send('Data received - dostal som všetky dáta potrebné na auth!');
+});
+
+// Pridaj ďalšie cesty, ktoré potrebuješ
+app.get('/auth/login', (req, res) => {
+  res.send('Toto je prihlásenie.');
 });
 
 // Spustenie HTTPS servera
