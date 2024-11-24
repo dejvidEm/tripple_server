12bea475d60036752884537e4d9ea4768dd210f8
dejvidEm <bithug10@gmail.com>
2024-11-24 17:37:05 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,16 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
+  // dostanem udaje od klienta na server
+  // overim ci uz email nieje registrovany -> vratim spravu na web nad register button
+  // ak neni tak registrujem uzivatela a vypisem mu na webe ze je uspesne zaregistrovany (ak splni kriteria pre heslo a mail a odskrtne captchu) a presmerujem ho na homepage
+
   const { fullName, email, password } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'Email is already registered' });
+      return res.status(409).json({ message: 'Email is already registered' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
