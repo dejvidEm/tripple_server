32eeb03ee7471dc6dac7e8e42536bb2cd707e819
dejvidEm <bithug10@gmail.com>
2024-11-24 19:39:41 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,6 +34,7 @@ exports.register = async (req, res) => {
     );
 
     res.status(201).json({ message: 'User registered successfully', token});
+    console.log(token);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
   }
